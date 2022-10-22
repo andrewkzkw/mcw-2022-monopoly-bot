3d92b4d79f268df750a89a5d066033888e26a8d3
Andrew <110312029+andrewkzkw@users.noreply.github.com>
2022-10-22 17:07:25 +0300

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -9,10 +9,10 @@ class GoogleSheet:
     def insert_row(self, row: list) -> None:
         self.sh[0].insert_rows(row=1, number=1, values=[row])
 
-    def get_records_by_telegram_id(self, telegram_id: str) -> dict | None:
+    def get_records_by_telegram_id(self, telegram_id: str) -> dict is not None:
         table = self.sh[1].get_all_values()[1::]
         for row in table:
             if row[0] != '':
                 if int(row[0]) == telegram_id:
-                    return {"Синий": row[1], "Красный": row[2], "Желтый": row[3]}
+                    return {"Желтые бренды: ": row[1], "Зеленые бренды: ": row[2], "Фиолетовые бренды: ": row[3]}
         return None
